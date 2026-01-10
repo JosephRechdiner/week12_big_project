424722336bc66fb1500f54a0b7f129a8a50bced8
JosephRechdiner <jrechdiner2002@gmail.com>
2026-01-10 17:54:53 +0200

    added dal layer and docker compose for testing

--- routes/browser_routes.py
+++ routes/browser_routes.py
@@ -1,28 +1,15 @@
 from fastapi import APIRouter, Depends
 from db.connector import MongoManager
-from utils import read_json
-import os
+from dal.queries import EventsDal
 
 browser_router = APIRouter()
 
 MONGO = MongoManager()
-MONGO_INITDB_DATABASE = os.getenv("MONGO_INITDB_DATABASE")
-COLLECTION = "events"
 
 @browser_router.get("/events/load_db")
 def load_events_to_db(client = Depends(MONGO.get_client)):
-    data = read_json()
-    db = client[MONGO_INITDB_DATABASE]
-    collection = db[COLLECTION]
-    collection.insert_many(data)
-    return {"msg": "ok"}
+    return EventsDal.add_events(client)
 
 @browser_router.get("/events/read_db")
-def get_events(client = Depends(MONGO.get_client)):
-    all_events = []
-    db = client[MONGO_INITDB_DATABASE]
-    collection = db[COLLECTION]
-    for event in collection.find({}):
-        event["_id"] = str(event["_id"])
-        all_events.append(event)
-    return all_events
+def get_events_from_db(client = Depends(MONGO.get_client)):
+    return EventsDal.get_events(client)
